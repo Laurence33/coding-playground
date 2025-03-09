dd679b8800a4baa32bc189198a0d90c167b9e563
Laurence Cortez <laurencecortez26.lc@gmail.com>
2025-03-09 09:31:46 +0800

    basics

--- nextjs/food-site/app/page.js
+++ nextjs/food-site/app/page.js
@@ -1,10 +1,10 @@
 import Link from 'next/link';
+import Header from '@/app/components/atoms/header';
 
 export default function Home() {
   return (
     <main>
-      <img src="/logo.png" alt="A server surrounded by magic sparkles." />
-      <h1>Welcome to NextJS!</h1>
+      <Header />
       <p>🔥 Let&apos;s get started! 🔥</p>
       <p><Link href="/about">About Us</Link></p>
     </main>
