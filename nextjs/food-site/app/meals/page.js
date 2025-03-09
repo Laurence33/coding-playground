81e5d5c62d15c5aead967ebdfe4e53518e2e6987
Laurence Cortez <laurencecortez26.lc@gmail.com>
2025-03-09 21:08:18 +0800

    fetch meals from db on meals page

--- nextjs/food-site/app/meals/page.js
+++ nextjs/food-site/app/meals/page.js
@@ -1,8 +1,11 @@
 import Link from "next/link";
 import classes from './page.module.css';
 import MealsGrid from "@/app/components/molecules/meals-grid/meals-grid";
+import { getMeals } from "@/lib/meals";
 
-export default function Meals() {
+export default async function Meals() {
+  const meals = await getMeals();
+
   return (
     <>
       <header className={classes.header}>
@@ -17,7 +20,7 @@ export default function Meals() {
         </p>
       </header>
       <main className={classes.main}>
-        <MealsGrid meals={[]} />
+        <MealsGrid meals={meals} />
       </main>
     </>
   )
