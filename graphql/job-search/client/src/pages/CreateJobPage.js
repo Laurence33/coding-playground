a3ebf8547dad907cf8614f04e52749d2e8035b15
Laurence Cortez <laurencecortez26.lc@gmail.com>
2025-03-21 02:33:01 +0800

    useMutation loading state

--- graphql/job-search/client/src/pages/CreateJobPage.js
+++ graphql/job-search/client/src/pages/CreateJobPage.js
@@ -7,7 +7,7 @@ function CreateJobPage() {
   const navigate = useNavigate();
   const [title, setTitle] = useState('');
   const [description, setDescription] = useState('');
-  const [mutate] = useMutation(createJobMutation);
+  const [mutate, { loading }] = useMutation(createJobMutation);
 
   const handleSubmit = async (event) => {
     event.preventDefault();
@@ -54,7 +54,11 @@ function CreateJobPage() {
           </div>
           <div className="field">
             <div className="control">
-              <button className="button is-link" onClick={handleSubmit}>
+              <button
+                className="button is-link"
+                onClick={handleSubmit}
+                disabled={loading}
+              >
                 Submit
               </button>
             </div>
