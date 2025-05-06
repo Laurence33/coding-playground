05c7072ee3e7f85369bbcc779ba143ea3d0443be
Laurence Cortez <laurencecortez26.lc@gmail.com>
2025-05-06 13:35:10 +0800

    not found pages

--- nextjs/news-app/app/news/[slug]/page.js
+++ nextjs/news-app/app/news/[slug]/page.js
@@ -1,8 +1,12 @@
+import { notFound } from "next/navigation";
 import { DUMMY_NEWS } from "@/dummy-news";
 
 export default function NewsDetailsPage({ params }) {
   const { slug } = params;
   const newsItem = DUMMY_NEWS.find(newsItem => newsItem.slug === slug);
+  if (!newsItem) {
+    notFound();
+  }
   return <>
     <article className="news-article">
       <header>
