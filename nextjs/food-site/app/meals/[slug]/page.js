39a07adf711b7a673872af8486f29e81da635c80
Laurence Cortez <laurencecortez26.lc@gmail.com>
2025-05-06 10:16:17 +0800

    dynamic metadata

--- nextjs/food-site/app/meals/[slug]/page.js
+++ nextjs/food-site/app/meals/[slug]/page.js
@@ -2,6 +2,17 @@ import Image from 'next/image';
 import classes from './page.module.css';
 import { getMeal } from '@/lib/meals';
 import { notFound } from 'next/navigation';
+export async function generateMetadata({ params }) {
+  const meal = getMeal(params.slug);
+  if (!meal) {
+    notFound();
+  }
+  return {
+    title: meal.title,
+    description: meal.summary
+  }
+}
+
 export default function MealPage({ params }) {
   const meal = getMeal(params.slug);
   if (!meal) {
