e48c2928789162e13de75acd7d89edb00695992d
Laurence Cortez <laurencecortez26.lc@gmail.com>
2025-05-06 13:20:31 +0800

    using dummy data and display to page with style

--- nextjs/news-app/app/news/[id]/page.js
+++
@@ -1,6 +0,0 @@
-export default function NewsDetailsPage({ params }) {
-  const { id } = params;
-  return <>
-    <h2>News: {id}</h2>
-  </>
-}
--- nextjs/news-app/app/news/page.js
+++ nextjs/news-app/app/news/page.js
@@ -1,14 +1,18 @@
+import { DUMMY_NEWS } from "@/dummy-news";
 import Link from "next/link";
 
 export default function NewsPage() {
-  const newsArr = ['news-1', 'news-2', 'news-3'];
   return <>
-    <div>
-      <ul>
-        {newsArr.map((el, index) => (
-          <li key={index}><Link href={`/news/${el}`}>{el}</Link></li>
-        ))}
-      </ul>
-    </div>
+    <h1>News Page</h1>
+    <ul className="news-list">
+      {DUMMY_NEWS.map((newsItem) => (
+        <li key={newsItem.id}>
+          <Link href={`/news/${newsItem.slug}`}>
+            <img src={`images/news/${newsItem.image}`} alt={newsItem.title} />
+            <span>{newsItem.title}</span>
+          </Link>
+        </li>
+      ))}
+    </ul>
   </>
 }
