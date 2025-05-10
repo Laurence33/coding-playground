0e035e5a0cdee301bb12de0ead30e6ad055b5477
Laurence Cortez <laurencecortez26.lc@gmail.com>
2025-05-10 10:55:11 +0800

    show list of animals, add one every btn click

--- react/animals-heart/src/AnimalShow.js
+++ react/animals-heart/src/AnimalShow.js
@@ -1,7 +1,23 @@
-import { useState } from "react";
 
+import bird from './assets/svg/bird.svg';
+import cat from './assets/svg/cat.svg';
+import cow from './assets/svg/cow.svg';
+import dog from './assets/svg/dog.svg';
+import gator from './assets/svg/gator.svg';
+import heart from './assets/svg/heart.svg';
+import horse from './assets/svg/horse.svg';
+
+const svgMap = {
+  bird,
+  cat,
+  cow,
+  dog,
+  gator, heart, horse
+}
 function AnimalShow({ type }) {
-  return <div>{type}</div>
+  return <div>
+    <img alt={type} src={svgMap[type]} />
+  </div>
 }
 
 export default AnimalShow;
