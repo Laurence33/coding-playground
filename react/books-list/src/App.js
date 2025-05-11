26f6b48671ebd2057bd88ee577fa79f6e13d7926
Laurence Cortez <laurencecortez26.lc@gmail.com>
2025-05-11 13:42:52 +0800

    integrate with api

--- react/books-list/src/App.js
+++ react/books-list/src/App.js
@@ -1,36 +1,48 @@
-import { useState } from 'react';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 import BookCreate from './components/BookCreate';
 import BookList from './components/BookList';
+const API_URL = import.meta.env.VITE_API_URL;
 
 export default function App() {
   const [books, setBooks] = useState([]);
-  const updateBookById = (id, newTitle) => {
+
+  useEffect(() => {
+    const getBooks = async () => {
+      const { data } = await axios.get(`${API_URL}/books`);
+      setBooks(data);
+    }
+    getBooks();
+  }, []);
+
+  const updateBookById = async (id, newTitle) => {
+    const { data } = await axios.put(`${API_URL}/books/${id}`, {
+      title: newTitle
+    });
+
     const updatedBooks = books.map(book => {
       if (book.id === id) {
-        return {
-          ...book,
-          title: newTitle
-        }
+        return data;
       }
       return book
     })
     setBooks(updatedBooks)
   }
-  const deleteBookById = (id) => {
+  const deleteBookById = async (id) => {
+    await axios.delete(`${API_URL}/books/${id}`);
     const updatedBooks = books.filter((book) => {
       return book.id !== id;
     })
     setBooks(updatedBooks);
   }
 
-  const createBook = (title) => {
+  const createBook = async (title) => {
+    const { data } = await axios.post(`${API_URL}/books`, { title });
+
     setBooks(prev => {
       return [
         ...prev,
-        {
-          id: Math.round(Math.random() * 999),
-          title
-        }
+        data
       ]
     })
   };
