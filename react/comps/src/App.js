c767687bd7e72ba082f140962d060a3cd65329ea
Laurence Cortez <laurencecortez26.lc@gmail.com>
2025-05-12 19:29:22 +0800

    Accordion component

--- react/comps/src/App.js
+++ react/comps/src/App.js
@@ -1,5 +1,24 @@
-export default function App() {
-  return <div>
+import Accordion from "./components/Accordion";
 
+export default function App() {
+  const items = [
+    {
+      id: 'abc',
+      label: 'Can I use React on a project?',
+      content: 'You can use React on any project you want. You can use react on any project you want.'
+    },
+    {
+      id: 'cde',
+      label: 'Can I use Javascript on a project?',
+      content: 'You can use React on any project you want. You can use react on any project you want.'
+    },
+    {
+      id: 'efg',
+      label: 'Can I use CSS on a project?',
+      content: 'You can use React on any project you want. You can use react on any project you want.'
+    }
+  ]
+  return <div>
+    <Accordion items={items} />
   </div>
 }
