70875f6d12677fd53c58f0f395c2a2a3e20b6fc9
Laurence Cortez <laurencecortez26.lc@gmail.com>
2025-05-12 17:55:04 +0800

    cleanup for accordion

--- react/comps/src/App.js
+++ react/comps/src/App.js
@@ -1,12 +1,5 @@
-import Button from "./components/Button";
-import { GoBell, GoCloudDownload, GoDatabase } from 'react-icons/go';
-
 export default function App() {
-  return <div>App
-    <Button className="mb-5" primary onClick={() => console.log("Hey!!!")}> <GoBell /> Primary</Button>
-    <Button secondary> <GoCloudDownload /> Secondary</Button>
-    <Button success outline> <GoDatabase /> Success</Button>
-    <Button warning>Warning</Button>
-    <Button danger rounded outline>Danger</Button>
+  return <div>
+
   </div>
 }
