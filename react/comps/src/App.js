3df6c05c4cf380737e5180680ba0c2abecff24e3
Laurence Cortez <laurencecortez26.lc@gmail.com>
2025-05-14 19:40:01 +0800

    dropdown component complete

--- react/comps/src/App.js
+++ react/comps/src/App.js
@@ -14,5 +14,6 @@ export default function App() {
   }
   return <div className="p-5">
     <Dropdown options={options} value={selection} onChange={handleChange} />
+    <Dropdown options={options} value={selection} onChange={handleChange} />
   </div>
 }
--- react/comps/src/components/Dropdown.js
+++ react/comps/src/components/Dropdown.js
@@ -1,10 +1,26 @@
-import { useState } from 'react';
+import { useEffect, useState, useRef } from 'react';
 import { GoChevronDown } from 'react-icons/go';
 import Panel from './Panel';
 
 function Dropdown({ options, value, onChange }) {
   const [isOpen, setIsOpen] = useState(false);
   const [selectedOption, setSelectedOption] = useState(value);
+  const divEl = useRef();
+
+  useEffect(() => {
+    const handler = (event) => {
+      if (!divEl.current) return;
+
+      if (!divEl.current.contains(event.target)) {
+        setIsOpen(false)
+      }
+    }
+    document.addEventListener('click', handler, true)
+
+    return () => {
+      document.removeEventListener('click');
+    }
+  }, [])
 
   const handleClick = () => {
     setIsOpen(prev => !prev);
@@ -20,7 +36,7 @@ function Dropdown({ options, value, onChange }) {
     return <div className="hover:bg-sky-100 rounded cursor-pointer p-1" onClick={() => handleOptionClick(option)} key={option.value}>{option.label}</div>
   })
 
-  return <div className="w-48 relative">
+  return <div ref={divEl} className="w-48 relative">
     <Panel
       className="flex justify-between items-center cursor-pointer"
       onClick={handleClick}>
