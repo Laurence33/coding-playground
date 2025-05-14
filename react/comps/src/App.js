8a1d20b8f06ec7e4568c73594ebb1452391cacb6
Laurence Cortez <laurencecortez26.lc@gmail.com>
2025-05-14 18:49:41 +0800

    dropdown component in progress

--- react/comps/src/App.js
+++ react/comps/src/App.js
@@ -1,24 +1,18 @@
-import Accordion from "./components/Accordion";
+import { useState } from 'react';
+import Dropdown from './components/Dropdown';
+
+const options = [
+  { label: 'Red', value: 'red' },
+  { label: 'Yellow', value: 'yellow' },
+  { label: 'Green', value: 'green' },
+]
 
 export default function App() {
-  const items = [
-    {
-      id: 'abc',
-      label: 'Can I use React on a project?',
-      content: 'You can use React on any project you want. You can use react on any project you want.'
-    },
-    {
-      id: 'cde',
-      label: 'Can I use Javascript on a project?',
-      content: 'You can use React on any project you want. You can use react on any project you want.'
-    },
-    {
-      id: 'efg',
-      label: 'Can I use CSS on a project?',
-      content: 'You can use React on any project you want. You can use react on any project you want.'
-    }
-  ]
+  const [selection, setSelection] = useState(null);
+  const handleChange = (value) => {
+    setSelection(value);
+  }
   return <div>
-    <Accordion items={items} />
+    <Dropdown options={options} value={selection} onChange={handleChange} />
   </div>
 }
