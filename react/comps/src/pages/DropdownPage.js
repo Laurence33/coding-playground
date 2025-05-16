860578193721f4793a78a3b1e42d7a4dec939329
Laurence Cortez <laurencecortez26.lc@gmail.com>
2025-05-16 09:38:57 +0800

    dropdown page

---
+++ react/comps/src/pages/DropdownPage.js
@@ -0,0 +1,20 @@
+
+import { useState } from 'react';
+import Dropdown from '../components/Dropdown';
+
+const options = [
+  { label: 'Red', value: 'red' },
+  { label: 'Yellow', value: 'yellow' },
+  { label: 'Green', value: 'green' },
+]
+
+export default function DropdownPage() {
+  const [selection, setSelection] = useState(null);
+  const handleChange = (value) => {
+    setSelection(value);
+  }
+  return <div className="p-5">
+    <Dropdown options={options} value={selection} onChange={handleChange} />
+    <Dropdown options={options} value={selection} onChange={handleChange} />
+  </div>
+}
