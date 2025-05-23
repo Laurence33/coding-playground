b5acebc5a672828cdbd00c0ebad77b9cfc972274
Laurence Cortez <laurencecortez26.lc@gmail.com>
2025-05-23 19:40:47 +0800

    simple form connected to redux

--- react/cars/src/components/CarForm.js
+++ react/cars/src/components/CarForm.js
@@ -1,3 +1,39 @@
+import { useDispatch, useSelector } from "react-redux";
+import { changeCost, changeName } from '../store';
+
 export default function CarForm() {
-  return <div>CarForm</div>
+  const form = useSelector((state) => state.form)
+  const dispatch = useDispatch();
+
+  const handleNameChange = (event) => {
+    dispatch(changeName(event.target.value));
+  }
+
+  const handleCostChange = (event) => {
+    const numberCost = parseInt(event.target.value) || 0;
+    dispatch(changeCost(numberCost));
+  }
+
+  return <div className="car-form-panel">
+    <h4 className="subtitle is-3">Add Car</h4>
+    <form>
+      <div className="field-group">
+        <div className="field">
+          <label className="label">Name</label>
+          <input className="input is-expanded"
+            value={form.name}
+            onChange={handleNameChange}
+          />
+        </div>
+
+        <div className="field">
+          <label className="label">Cost</label>
+          <input className="input is-expanded"
+            value={form.cost || ''}
+            onChange={handleCostChange}
+          />
+        </div>
+      </div>
+    </form>
+  </div>
 }
