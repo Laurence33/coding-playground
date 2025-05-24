882e8a06ffc2ca05684a6ea5cf00f6b10e922478
Laurence Cortez <laurencecortez26.lc@gmail.com>
2025-05-25 05:25:38 +0800

    createSelector

--- react/cars/src/components/CarList.js
+++ react/cars/src/components/CarList.js
@@ -1,8 +1,16 @@
 import { useDispatch, useSelector } from "react-redux"
+import { createSelector } from "@reduxjs/toolkit";
 import { removeCar } from "../store";
 
+const memoizedCars = createSelector(
+  [(state) => state.cars.list, (state) => state.cars.searchTerm],
+  (cars, searchTerm) => {
+    return cars.filter(car => car.name.toLowerCase().includes(searchTerm.toLowerCase()))
+  }
+)
+
 export default function CarList() {
-  const cars = useSelector(state => state.cars.list);
+  const cars = useSelector(memoizedCars);
   const dispatch = useDispatch();
 
   const handleCarDelete = (car) => {
