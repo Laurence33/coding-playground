014ee4431784e9d71687c6c5a78742a8c5c83784
Laurence Cortez <laurencecortez26.lc@gmail.com>
2025-05-25 05:50:41 +0800

    memoized totalCost using createSelector

--- react/cars/src/components/CarValue.js
+++ react/cars/src/components/CarValue.js
@@ -1,3 +1,18 @@
+import { createSelector } from '@reduxjs/toolkit';
+import { useSelector } from 'react-redux';
+
+const memoizedTotalCost = createSelector(
+  [(state) => state.cars.list, (state) => state.cars.searchTerm],
+  (cars, searchTerm) => {
+    const filteredCars = cars.filter(car => car.name.toLowerCase().includes(searchTerm.toLowerCase()));
+    const totalCost = filteredCars.reduce((acc, car) => acc + car.cost, 0);
+    return totalCost;
+  }
+);
+
 export default function CarValue() {
-  return <div>CarValue</div>
+  const totalCost = useSelector(memoizedTotalCost)
+  return <div className="car-value">
+    Total Cost: ${totalCost}
+  </div>
 }
