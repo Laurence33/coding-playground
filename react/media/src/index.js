87ceabbd3eb18836bb764fa570c3ecf09a5246b3
Laurence Cortez <laurencecortez26.lc@gmail.com>
2025-05-25 06:23:00 +0800

    initial setup with tailwind

--- react/media/src/index.js
+++ react/media/src/index.js
@@ -1,11 +1,8 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
 import './index.css';
+import { createRoot } from 'react-dom/client';
 import App from './App';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const el = document.getElementById('root');
+const app = createRoot(el);
+
+app.render(<App />);
