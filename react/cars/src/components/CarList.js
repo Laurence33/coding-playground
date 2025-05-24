67a17d53b0b772e14ae97e34b1c2b8031e93f604
Laurence Cortez <laurencecortez26.lc@gmail.com>
2025-05-24 18:11:19 +0800

    remove car

--- react/cars/src/components/CarList.js
+++ react/cars/src/components/CarList.js
@@ -1,4 +1,27 @@
+import { useDispatch, useSelector } from "react-redux"
+import { removeCar } from "../store";
 
 export default function CarList() {
-  return <div>CarList</div>
+  const cars = useSelector(state => state.cars.list);
+  const dispatch = useDispatch();
+
+  const handleCarDelete = (car) => {
+    dispatch(removeCar(car.id));
+  }
+  const renderedCars = cars.map(car => {
+    return (
+      <div key={car.id} className="panel">
+        <p>
+          {car.name} - ${car.cost}
+        </p>
+        <button
+          onClick={() => handleCarDelete(car)}
+          className="button is-danger">Delete</button>
+      </div>
+    )
+  })
+  return <div className="car-list">
+    {renderedCars}
+    <hr />
+  </div>
 }
