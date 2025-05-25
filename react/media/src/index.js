84f7f8747835bc6a3222d54f693d650e7c0514d8
Laurence Cortez <laurencecortez26.lc@gmail.com>
2025-05-25 08:58:18 +0800

    initial redux toolkit setup

--- react/media/src/index.js
+++ react/media/src/index.js
@@ -1,8 +1,14 @@
 import './index.css';
 import { createRoot } from 'react-dom/client';
 import App from './App';
+import { Provider } from 'react-redux';
+import { store } from './store';
 
 const el = document.getElementById('root');
 const app = createRoot(el);
 
-app.render(<App />);
+app.render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+);
