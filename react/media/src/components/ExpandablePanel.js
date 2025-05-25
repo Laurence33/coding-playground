1a1172a1c99b81a3d71b22d115d6a009978d2c9d
Laurence Cortez <laurencecortez26.lc@gmail.com>
2025-05-26 07:32:07 +0800

    minor knits

--- react/media/src/components/ExpandablePanel.js
+++ react/media/src/components/ExpandablePanel.js
@@ -12,7 +12,7 @@ function ExpandablePanel({ header, children }) {
   return (
     <div className="mb-2 border rounded">
       <div className="flex p-2 justify-between items-center">
-        <div className="flex flex-row items-center justify-between">
+        <div className="flex items-center justify-between">
           {header}
         </div>
         <div className="cursor-pointer " onClick={handleClick}>
