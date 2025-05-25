1830450dac0ad3fe17bd44d76690104808febed3
Laurence Cortez <laurencecortez26.lc@gmail.com>
2025-05-25 09:55:52 +0800

    using the thunk in component

--- react/media/src/components/UsersList.js
+++ react/media/src/components/UsersList.js
@@ -1,14 +1,24 @@
 import { useEffect } from "react";
 import { fetchUsers } from "../store";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 
 function UsersList() {
   const dispatch = useDispatch();
+  const { data, isLoading, error } = useSelector(state => state.users);
+
   useEffect(() => {
     dispatch(fetchUsers());
   }, [dispatch])
 
-  return 'users list'
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  if (error) {
+    return <div>Error fetching data! {error.message}</div>
+  }
+
+  return data.length;
 }
 
 export default UsersList;
