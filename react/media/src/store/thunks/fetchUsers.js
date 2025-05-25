53436d7526b230d2f7638c30b70887436169c0b2
Laurence Cortez <laurencecortez26.lc@gmail.com>
2025-05-25 09:55:39 +0800

    add delay for adding spinner

--- react/media/src/store/thunks/fetchUsers.js
+++ react/media/src/store/thunks/fetchUsers.js
@@ -3,7 +3,15 @@ import axios from 'axios';
 
 const fetchUsers = createAsyncThunk('users/fetch', async () => {
   const response = await axios.get('http://localhost:3005/users');
+  // DEV ONLY!!
+  await sleep(1000);
   return response.data;
 });
 
+// DEV ONLY!!
+const sleep = (duration) => {
+  return new Promise((resolve) => {
+    setTimeout(resolve, duration);
+  })
+}
 export { fetchUsers };
